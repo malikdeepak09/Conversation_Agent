92a987b343aa29aaa7bc02d6010e6bd4f182e777
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2026-01-29 20:52:13 +0530

    deleted embedding file

--- embedding.py
+++
@@ -1,9 +0,0 @@
-# Embedding generation  
-from sentence_transformers import SentenceTransformer
-
-class EmbeddingGenerator:
-    def __init__(self, model_name='all-MiniLM-L6-v2'):
-        self.model = SentenceTransformer(model_name)
-
-    def generate_embedding(self, text: str):
-        return self.model.encode(text)
